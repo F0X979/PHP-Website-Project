3e6344976029af53a27599ab1a850564b47ce9c2
F0X979 <f0x979c0@gmail.com>
2015-08-24 11:14:21 +0300

    Create profile.php

---
+++ profile.php
@@ -0,0 +1,30 @@
+<?php 
+
+session_start();
+
+include 'inc/connect.php';
+    
+    
+    
+    
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+
+
+<link rel="stylesheet" type="text/css" href="">
+
+
+
+</head>
+
+<body>
+
+
+</body>
+
+</html> 
